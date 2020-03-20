61384038d4a5e80b13c07e7c5b974847dcfc9c5c
LllC-mmd <951785875@qq.com>
2020-03-20 08:57:01 +0800

    Update hw2_LDA.py

--- AML/HW2/hw2_LDA.py
+++ AML/HW2/hw2_LDA.py
@@ -1,3 +1,5 @@
+import sys
+import timeit
 import numpy as np
 from scipy.special import gamma
 from sklearn.feature_extraction.text import CountVectorizer
@@ -36,7 +38,7 @@ class ldaModel(object):
         self.a = a
         self.b = b
 
-    def fit(self, data, Vmax, max_iter, n_burn, hyper_update=False):
+    def fit(self, data, Vmax, n_sample, n_burn, hyper_update=False):
         # data: word frequency matrix for the corpus
         # where data[m, t] = the amount of t-th words in m-th document
         M = len(data)
@@ -58,37 +60,14 @@ class ldaModel(object):
                 c_topic[m, tp_new-1] += 1
                 c_word[tp_new-1, n] += 1
 
-        #c_topic = np.array([np.bincount(tpm, minlength=self.K+1)[1:] for tpm in Z])
-        #c_word = np.array([np.bincount(wt, minlength=self.K+1)[1:] for wt in Z.reshape(Vmax, M)]).transpose()
-
         # initialize the hyper-parameters
         alpha = np.ones(self.K)*50/self.K
         beta = np.ones(Vmax)*0.01
         topic_set = np.array([i for i in range(1, self.K+1)])
 
-        # infer hidden variables and learn parameters by EM algorithm
-        for iter in range(0, max_iter):
-            # Fill Z by Gibbs sampling from posterior distribution
-            # ---Start the burn-in process of Gibbs sampling
-            for t_burn in range(0, n_burn):
-                for m in range(0, M):
-                    words_m = np.nonzero(data[m])[0]
-                    for w_id in words_m:
-                        w_tp = Z[m, w_id]
-                        c_topic[m, w_tp-1] -= 1
-                        c_word[w_tp-1, w_id] -= 1
-                        # ------select a new topic
-                        #z_cword = np.array([c_word[k, w_id] for k in range(0, self.K)])
-                        #print(z_cword)
-                        z_pval = np.array([(c_word[k, w_id]+beta[w_id])*(c_topic[m, k]+alpha[k])/(np.sum(c_word[k, :])+np.sum(beta)-1)
-                                           for k in range(0, self.K)])
-                        z_pval = z_pval/np.sum(z_pval)
-                        tp_new = np.random.choice(topic_set, 1, p=z_pval)[0]
-                        Z[m, w_id] = tp_new
-                        # ------update the counter matrix
-                        c_topic[m, tp_new-1] += 1
-                        c_word[tp_new-1, w_id] += 1
-            # ---sample for Z
+        # Fill Z by Gibbs sampling from posterior distribution
+        # ---Start the burn-in process of Gibbs sampling
+        for t_burn in range(0, n_burn):
             for m in range(0, M):
                 words_m = np.nonzero(data[m])[0]
                 for w_id in words_m:
@@ -96,7 +75,39 @@ class ldaModel(object):
                     c_topic[m, w_tp-1] -= 1
                     c_word[w_tp-1, w_id] -= 1
                     # ------select a new topic
-                    z_pval = np.array([(c_word[k, w_id]+beta[w_id])*(c_topic[m, k]+alpha[k])/(np.sum(c_word[k, :])+np.sum(beta)-1)
+                    z_pval = np.array([(c_word[k, w_id]+beta[w_id])*(c_topic[m, k]+alpha[k])/(np.sum(c_word[k, :])+np.sum(beta))
+                                        for k in range(0, self.K)])
+                    z_pval = z_pval/np.sum(z_pval)
+                    tp_new = np.random.choice(topic_set, 1, p=z_pval)[0]
+                    Z[m, w_id] = tp_new
+                    # ------update the counter matrix
+                    c_topic[m, tp_new-1] += 1
+                    c_word[tp_new-1, w_id] += 1
+                    # Update the hyper-parameters
+                    if hyper_update:
+                        for k in range(0, self.K):
+                            alpha[k] = abSampler(x0=alpha[k], x_id=k, count_mat=c_topic, n_burn=1000, a=self.a, b=self.b)
+                        for v in range(0, Vmax):
+                            beta[v] = abSampler(x0=beta[v], x_id=v, count_mat=c_word, n_burn=1000, a=self.a, b=self.b)
+            # Fill Theta and estimate Phi with their expectation
+            if (t_burn+1) % 10 == 0:
+                Theta = c_topic + alpha
+                Theta = np.transpose(Theta.transpose() / np.sum(Theta, axis=1))
+                Phi = c_word + beta
+                Phi = np.transpose(Phi.transpose() / np.sum(Phi, axis=1))
+                # report the log-likelihood for documents
+                print("The log-likelihood at iteration ", t_burn+1, ": ", self.score(data, Theta, Phi))
+
+        # ---sampling from the distribution after burn-in
+        for t_sample in range(0, n_sample):
+            for m in range(0, M):
+                words_m = np.nonzero(data[m])[0]
+                for w_id in words_m:
+                    w_tp = Z[m, w_id]
+                    c_topic[m, w_tp-1] -= 1
+                    c_word[w_tp-1, w_id] -= 1
+                    # ------select a new topic
+                    z_pval = np.array([(c_word[k, w_id]+beta[w_id])*(c_topic[m, k]+alpha[k])/(np.sum(c_word[k, :])+np.sum(beta))
                                        for k in range(0, self.K)])
                     z_pval = z_pval / np.sum(z_pval)
                     tp_new = np.random.choice(topic_set, 1, p=z_pval)[0]
@@ -104,19 +115,13 @@ class ldaModel(object):
                     # ------update the counter matrix
                     c_topic[m, tp_new-1] += 1
                     c_word[tp_new-1, w_id] += 1
-            # Fill Theta and estimate Phi with their expectation
-            Theta = c_topic + alpha
-            Theta = np.reshape(Theta.reshape(self.K, M) / np.sum(Theta, axis=1), (M, self.K))
-            Phi = c_word + beta
-            Phi = np.reshape(Phi.reshape(Vmax, self.K) / np.sum(Phi, axis=1), (self.K, Vmax))
-            # Update the hyper-parameters
-            if hyper_update:
-                for k in range(0, self.K):
-                    alpha[k] = abSampler(x0=alpha[k], x_id=k, count_mat=c_topic, n_burn=1000, a=self.a, b=self.b)
-                for v in range(0, Vmax):
-                    beta[v] = abSampler(x0=beta[v], x_id=v, count_mat=c_word, n_burn=1000, a=self.a, b=self.b)
-            # report the log-likelihood for documents
-            print("The log-likelihood at iteration ", iter, ": ", self.score(data, Theta, Phi))
+
+        Theta = c_topic + alpha
+        Theta = np.transpose(Theta.transpose() / np.sum(Theta, axis=1))
+        Phi = c_word + beta
+        Phi = np.transpose(Phi.transpose() / np.sum(Phi, axis=1))
+        # report the log-likelihood for documents
+        print("The final log-likelihood: ", self.score(data, Theta, Phi))
 
         return Z, Phi
 
@@ -125,9 +130,7 @@ class ldaModel(object):
         M = len(Theta)
         for m in range(0, M):
             words_m = np.nonzero(data[m])[0]
-            Nm = len(words_m)
-            for n in range(0, Nm):
-                w_id = words_m[n]
+            for w_id in words_m:
                 s += np.log(np.sum([Theta[m, k]*Phi[k, w_id] for k in range(0, self.K)]))
         return s
 
@@ -141,7 +144,17 @@ X = vectorizer.fit_transform(corpus)
 vocabulary = vectorizer.get_feature_names()
 word_frequency = X.toarray()  # shape: (10768, 6986)
 
-model = ldaModel(n_topics=3, a=0.5, b=0.5)
+model = ldaModel(n_topics=int(sys.argv[1]), a=0.5, b=0.5)
 
-np.random.seed(111)
-model.fit(data=word_frequency, Vmax=len(vocabulary), max_iter=100, n_burn=1)
+#np.random.seed(111)
+start = timeit.default_timer()
+z_fit, phi_fit = model.fit(data=word_frequency, Vmax=len(vocabulary), n_sample=100, n_burn=4000)
+stop = timeit.default_timer()
+print("Run-time of Fitting: ", stop - start)
+print(" ")
+print("Topic Explanation")
+for k in range(0, model.K):
+    print("********Topic ", k, "********")
+    word_max = phi_fit[k, :].argsort()[-10:][::-1]
+    for w_id in word_max:
+        print(vocabulary[w_id], ": ", phi_fit[k, w_id])
