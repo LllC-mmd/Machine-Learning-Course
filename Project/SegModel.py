0bada7960b2851007e43945ada09cb039e0c5e8e
LllC-mmd <951785875@qq.com>
2020-06-02 08:53:28 +0800

    Update SegModel.py

--- Project/SegModel.py
+++ Project/SegModel.py
@@ -42,7 +42,7 @@ class Decoder(nn.Module):
 
 
 class LUSegDeepLab(nn.Module):
-    def __init__(self, backbone, num_plane=2048, output_stride=16, num_classes=5, pretrained_backbone=None):
+    def __init__(self, backbone, num_plane=2048, output_stride=8, num_classes=5, pretrained_backbone=None):
         super(LUSegDeepLab, self).__init__()
         self.backbone = build_backbone(backbone, output_stride, pretrained_backbone)
         self.aspp = AtrousSPP(in_plane=num_plane, output_stride=output_stride, num_plane=256)
@@ -106,4 +106,49 @@ class LUSegUNet(nn.Module):
         return out
 
 
+class LUSegUNet_IN(nn.Module):
+    def __init__(self, num_channel=512, num_classes=5):
+        super(LUSegUNet_IN, self).__init__()
+        self.conv_ini = DownConvIN(3, int(num_channel / 16), IN=True)
+        self.mpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.down_conv1 = DownConvIN(int(num_channel / 16), int(num_channel / 8), IN=True)
+        self.mpool2 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.down_conv2 = DownConvIN(int(num_channel / 8), int(num_channel / 4), IN=False)
+        self.mpool3 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.down_conv3 = DownConvIN(int(num_channel / 4), int(num_channel / 2), IN=False)
+        self.mpool4 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.down_conv4 = DownConvIN(int(num_channel / 2), num_channel, IN=False)
 
+        self.up_conv4 = UpConv(in_plane=num_channel, skip_plane=int(num_channel / 2), num_plane=int(num_channel / 2))
+        self.up_conv3 = UpConv(in_plane=int(num_channel / 2), skip_plane=int(num_channel / 4),
+                               num_plane=int(num_channel / 4))
+        self.up_conv2 = UpConv(in_plane=int(num_channel / 4), skip_plane=int(num_channel / 8),
+                               num_plane=int(num_channel / 8))
+        self.up_conv1 = UpConv(in_plane=int(num_channel / 8), skip_plane=int(num_channel / 16),
+                               num_plane=int(num_channel / 16))
+
+        self.out_conv = nn.Conv2d(int(num_channel / 16), num_classes, kernel_size=1)
+
+    def forward(self, x):
+        x0 = self.conv_ini(x)  # 224
+
+        x1 = self.mpool1(x0)  # 112
+        x1 = self.down_conv1(x1)
+
+        x2 = self.mpool2(x1)  # 56
+        x2 = self.down_conv2(x2)
+
+        x3 = self.mpool3(x2)  # 28
+        x3 = self.down_conv3(x3)
+
+        x4 = self.mpool4(x3)  # 14
+        x4 = self.down_conv4(x4)
+
+        out = self.up_conv4(x4, x3)
+        out = self.up_conv3(out, x2)
+        out = self.up_conv2(out, x1)
+        out = self.up_conv1(out, x0)
+
+        out = self.out_conv(out)
+
+        return out
